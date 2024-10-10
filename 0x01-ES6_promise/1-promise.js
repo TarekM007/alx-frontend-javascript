96e691b43afc4865a7f829f23ff9d5b5ab1dae42
TarekM007 <tarekmoustafa36@gmail.com>
2024-10-10 15:39:29 +0000

    1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'success' });
+    } else {
+      reject(new Error(' The fake API is not working currently'));
+    }
+  });
+}
+
+export default getResponseFromAPI;
