97a7bc245510d712c6c9c0ba628c21d83cde36ee
TarekM007 <tarekmoustafa36@gmail.com>
2024-10-10 15:25:54 +0000

    0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,6 @@
 function getResponseFromAPI() {
+  // eslint-disable-next-line
   return new Promise((resolve, reject) => {
-    // Simulate an API response with a timeout
-    setTimeout(() => {
-      const success = true; // Change this to false to simulate an error
-      if (success) {
-        resolve('API response received!');
-      } else {
-        reject(new Error('API call failed.'));
-      }
-    }, 1000); // Simulate a 1-second delay
   });
 }
 
