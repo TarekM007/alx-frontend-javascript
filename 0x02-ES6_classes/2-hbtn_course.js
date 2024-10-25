08922d31a622e8f37e4e5409bf32f1d5519e4e7a
TarekM007 <tarekmoustafa36@gmail.com>
2024-10-25 18:17:30 +0000

    2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  // name getter and setter
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  // length getter and setter
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    this._length = length;
+  }
+
+  // students getter and setter
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = students;
+  }
+}
