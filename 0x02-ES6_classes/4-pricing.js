06b925749ce39ca79cb95aa1922417e7e171a1a0
TarekM007 <tarekmoustafa36@gmail.com>
2024-11-01 14:12:38 +0000

    4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // name getter and setter
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  // length getter and setter
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
